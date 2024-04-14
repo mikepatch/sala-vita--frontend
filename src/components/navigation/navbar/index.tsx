277997a47a414c78a032e42bafd7dccf89be432b
Michal Lata <michallata.dev@gmail.com>
2024-04-14 17:35:19 +0200

    patch: Update Navbar styles

--- src/components/navigation/navbar/index.tsx
+++ src/components/navigation/navbar/index.tsx
@@ -11,7 +11,7 @@ type NavbarProps = {
 
 export const Navbar = ({ isOpen, toggle }: Readonly<NavbarProps>) => {
 	const [isScrolled, setIsScrolled] = useState(false);
-	const hamburgerLineClassName = `block absolute h-0.5 w-6 rounded-md transition ease transform duration-300 ${isScrolled || isOpen ? "bg-brand-primary" : "bg-white"}`;
+	const hamburgerLineClassName = `block absolute h-0.5 w-7 rounded-md transition ease-in-out transform duration-500 ${isScrolled || isOpen ? "bg-brand-primary" : "bg-brand-primary"}`;
 
 	useEffect(() => {
 		const handleScroll = () => {
@@ -30,43 +30,46 @@ export const Navbar = ({ isOpen, toggle }: Readonly<NavbarProps>) => {
 	}, []);
 
 	return (
-		<div
-			className={`fixed left-0 top-0 z-50 flex w-full justify-between p-4 text-white transition-colors ${isScrolled && !isOpen && "!text-brand-primary bg-white shadow-md"}`}
+		<nav
+			className={`fixed left-0 top-0 z-50 flex h-[80px] w-full items-center justify-between px-8 py-4 text-brand-primary transition-colors ${isScrolled && !isOpen && "bg-white !text-brand-primary shadow-md"}`}
 		>
-			<div>
-				<Link href="/" className={`hidden ${isScrolled && !isOpen && "block"}`}>
-					<NextImage src="/logo-normal.png" alt="Logo" width={50} height={50} />
-				</Link>
-			</div>
-			<button
-				type="button"
-				onClick={toggle}
-				className={`relative z-50 h-10 w-10 md:hidden ${isScrolled || isOpen ? "text-brand-primary" : "text-white"}`}
-				aria-label="Open menu"
-			>
-				<span className="sr-only">Open menu</span>
-				<div className="absolute left-1/2 top-1/2 block w-5 -translate-x-1/2 -translate-y-1/2 transform">
-					<span
-						aria-hidden="true"
-						className={`${hamburgerLineClassName} ${isOpen ? "rotate-45" : "-translate-y-1.5"}`}
-					></span>
-					<span
-						aria-hidden="true"
-						className={`${hamburgerLineClassName} ${isOpen && "opacity-0"}`}
-					></span>
-					<span
-						aria-hidden="true"
-						className={`${hamburgerLineClassName} ${isOpen ? "-rotate-45" : "translate-y-1.5"}`}
-					></span>
+			<div className="mx-auto flex w-full max-w-5xl items-center justify-between">
+				<div>
+					<Link href="/" className={`hidden h-full ${isScrolled && !isOpen && "!block"}`}>
+						<NextImage src="/logo-normal.png" alt="Logo" width={50} height={50} />
+					</Link>
 				</div>
-			</button>
-			<ul className="hidden gap-4 md:flex">
-				{NAV_ITEMS.map((item) => (
-					<li key={item.href}>
-						<Link href={item.href}>{item.label}</Link>
-					</li>
-				))}
-			</ul>
-		</div>
+				<button
+					type="button"
+					onClick={toggle}
+					className={`relative z-50 flex aspect-square h-12 items-center justify-center overflow-hidden md:hidden`}
+					aria-label="Open menu"
+					tabIndex={0}
+				>
+					<span className="sr-only">Open menu</span>
+					<div className="relative w-7">
+						<span
+							aria-hidden="true"
+							className={`${hamburgerLineClassName} ${isOpen ? "rotate-45" : "-translate-y-1.5"}`}
+						></span>
+						<span
+							aria-hidden="true"
+							className={`${hamburgerLineClassName} ${isOpen && "translate-x-full opacity-0"}`}
+						></span>
+						<span
+							aria-hidden="true"
+							className={`${hamburgerLineClassName} ${isOpen ? "-rotate-45" : "translate-y-1.5"}`}
+						></span>
+					</div>
+				</button>
+				<ul className="hidden gap-4 md:flex">
+					{NAV_ITEMS.map((item) => (
+						<li key={item.href}>
+							<Link href={item.href}>{item.label}</Link>
+						</li>
+					))}
+				</ul>
+			</div>
+		</nav>
 	);
 };
