ba901f9e4a76c2ba65322ee7fa65c83d0413cfb0
Michal Lata <michallata.dev@gmail.com>
2024-04-14 17:33:37 +0200

    feat: Add ActiveLink component

---
+++ src/components/common/ActiveLink.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { type Route } from "next";
+
+type ActiveLinkProps = {
+	children: React.ReactNode;
+	href: Route;
+	className?: string;
+	disabled?: boolean;
+	activeClassName?: string;
+	target?: "_self" | "_blank";
+	ariaLabel?: string;
+	onClick?: () => void;
+};
+
+export const ActiveLink = ({
+	children,
+	href,
+	className = "",
+	disabled = false,
+	activeClassName = "",
+	target = "_self",
+	ariaLabel = "",
+	onClick,
+}: ActiveLinkProps) => {
+	const pathname = usePathname();
+	const isActive = pathname === href;
+
+	return (
+		<Link
+			aria-current={isActive ? "page" : undefined}
+			href={`${href}` as Route}
+			className={`${className}, ${isActive && activeClassName}`}
+			target={target}
+			aria-label={ariaLabel}
+			aria-disabled={disabled}
+			onClick={onClick}
+		>
+			{children}
+		</Link>
+	);
+};
