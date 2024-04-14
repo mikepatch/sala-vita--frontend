ed5bc11a91840f288803d5f00cb9def72a77dd84
Michal Lata <michallata.dev@gmail.com>
2024-04-14 17:35:31 +0200

    feat: Add NAV_ITEMS constant

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export const NAV_ITEMS = [
+	{ href: "/", label: "Strona główna" },
+	{ href: "/about", label: "O nas" },
+	{ href: "/oferta", label: "Oferta" },
+	{ href: "/gallery", label: "Galeria" },
+	{ href: "/reservations", label: "Rezerwacje" },
+	{ href: "/contact", label: "Kontakt" },
+];
