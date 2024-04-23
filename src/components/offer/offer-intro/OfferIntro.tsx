3b7c4f6c10d4f00b85296a9de02db5a472941236
Michal Lata <michallata.dev@gmail.com>
2024-04-23 18:44:42 +0200

    patch: Refactor OfferIntro

--- src/components/offer/offer-intro/OfferIntro.tsx
+++ src/components/offer/offer-intro/OfferIntro.tsx
@@ -1,50 +1,26 @@
-import NextImage from "next/image";
-import Link from "next/link";
-import { Gem } from "lucide-react";
-
-import { OFFER_ITEMS } from "@/constants";
+import sectionImg from "../../../../public/images/6.jpg";
+import { LinkButton } from "@/components/common/LinkButton";
+import { IntroSectionImage } from "@/components/common/IntroSectionImage";
+import { IntroSectionHeader } from "@/components/common/IntroSectionHeader";
+import { IntroSectionContainer } from "@/components/common/IntroSectionContainer";
+import { OfferIntroFeatureList } from "@/components/offer/offer-intro/OfferIntroFeatureList";
 
 export const OfferIntro = () => {
 	return (
-		<section className="container flex flex-col gap-6 xl:flex-row-reverse xl:justify-between xl:gap-12">
+		<IntroSectionContainer layout="row-reverse">
 			<article className="flex flex-col px-8 xl:max-w-lg">
-				<header>
-					<h2 className="py-6 text-4xl font-bold text-brand-primary">Oferta</h2>
-				</header>
+				<IntroSectionHeader title="Oferta" />
 				<div className="flex flex-col gap-6">
 					<p>
 						W <strong>VITA</strong> oferujemy kompleksową organizację wydarzeń okolicznościowych.
 					</p>
-					<ul className="flex flex-col gap-6">
-						{OFFER_ITEMS.map(({ id, title, shortDescription }) => (
-							<li key={id} className="flex items-center gap-4">
-								<Gem strokeWidth={2} className="" />
-								<article className="basis-5/6">
-									<header>
-										<h3 className="text-xl font-bold text-brand-primary">{title}</h3>
-									</header>
-									<p>{shortDescription}</p>
-								</article>
-							</li>
-						))}
-					</ul>
-					<Link
-						href="/oferta"
-						className="self-start rounded-sm border border-brand-primary border-opacity-60 bg-white px-6 py-3 text-center text-brand-primary shadow-sm transition-colors"
-					>
+					<OfferIntroFeatureList />
+					<LinkButton href="/oferta" className="self-start">
 						Sprawdź szczegóły
-					</Link>
+					</LinkButton>
 				</div>
 			</article>
-			<figure className="h-[600px] max-w-full overflow-hidden xl:basis-1/2 xl:rounded-sm">
-				<NextImage
-					src="/6.jpg"
-					alt="2"
-					width={1500}
-					height={1500}
-					className="h-full w-full animate-fade-right object-cover object-center"
-				/>
-			</figure>
-		</section>
+			<IntroSectionImage img={sectionImg} alt="Stół z zastawą" />
+		</IntroSectionContainer>
 	);
 };
