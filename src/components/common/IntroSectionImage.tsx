660f99575ac65f6b650669ade2e8a2f9225d841c
Michal Lata <michallata.dev@gmail.com>
2024-04-23 18:42:49 +0200

    feat: Add IntroSectionImage

---
+++ src/components/common/IntroSectionImage.tsx
@@ -0,0 +1,22 @@
+import NextImage, { type StaticImageData } from "next/image";
+
+type IntroSectionImageProps = {
+	img: StaticImageData;
+	alt: string;
+};
+
+export const IntroSectionImage = ({ img, alt }: IntroSectionImageProps) => {
+	return (
+		<figure className="relative h-[600px] w-full overflow-hidden xl:basis-1/2 xl:rounded-sm">
+			<NextImage
+				src={img}
+				alt={alt}
+				placeholder="blur"
+				loading="lazy"
+				fill
+				className="object-cover object-center"
+				sizes="(max-width: 1280px) 100vw, 768px"
+			/>
+		</figure>
+	);
+};
