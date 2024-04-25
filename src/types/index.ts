2bf98ddb806f95be7692cdc5df53a2bfd71cabf7
Michal Lata <michallata.dev@gmail.com>
2024-04-25 15:30:44 +0200

    feat: Add OfferItem and ContactItem types

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type TOfferItem = {
+	id: string;
+	title: string;
+	shortDescription: string;
+};
+
+export type TContactItem = {
+	icon: JSX.Element;
+	title: string;
+	content: string;
+	href: string;
+};
