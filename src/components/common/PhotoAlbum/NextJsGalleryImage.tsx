9e422756e911d0b46c3794b1d0b09c5b3f7e2af1
Michal Lata <michallata.dev@gmail.com>
2024-05-04 18:21:01 +0200

    feat: Add NextJsGalleryImage

---
+++ src/components/common/PhotoAlbum/NextJsGalleryImage.tsx
@@ -0,0 +1,21 @@
+import { type RenderPhotoProps } from "react-photo-album";
+import NextImage from "next/image";
+import clsx from "clsx";
+
+export const NextJsGalleryImage = ({
+	photo,
+	imageProps: { alt, title, sizes, className, onClick },
+	wrapperStyle,
+}: RenderPhotoProps) => {
+	return (
+		<figure style={{ ...wrapperStyle, position: "relative" }}>
+			<NextImage
+				fill
+				src={photo}
+				placeholder={"blurDataURL" in photo ? "blur" : undefined}
+				className={clsx(className, "object-cover object-center transition-all hover:brightness-90")}
+				{...{ alt, title, sizes, onClick }}
+			/>
+		</figure>
+	);
+};
