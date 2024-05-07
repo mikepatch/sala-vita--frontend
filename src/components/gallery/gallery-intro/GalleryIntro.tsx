bde9e81515c70a8444b49891e45ec3e84a3a4bd5
Michal Lata <michallata.dev@gmail.com>
2024-05-07 20:09:25 +0200

    patch: Update GalleryIntro

--- src/components/gallery/gallery-intro/GalleryIntro.tsx
+++ src/components/gallery/gallery-intro/GalleryIntro.tsx
@@ -6,20 +6,19 @@ import { PhotoGallery } from "@/components/common/PhotoAlbum/PhotoGallery";
 
 export const GalleryIntro = () => {
 	return (
-		<IntroSectionContainer>
-			<article className="flex w-full flex-col px-8 xl:items-center">
+		<IntroSectionContainer layout="row">
+			<article className="flex flex-col px-8 lg:max-w-lg">
 				<IntroSectionHeader title="Galeria" />
-				<div className="flex flex-col items-center gap-6">
-					<p>
-						Zapoznaj się z naszą galerią, aby zobaczyć jak wygląda nasza sala bankietowa oraz
-						wydarzenia, które w niej organizujemy.
-					</p>
-					<div className="w-full">
-						<PhotoGallery photos={GALLERY_IMAGES.slice(0, 6)} />
-					</div>
-					<LinkButton href="/galeria">Zobacz całą galerię</LinkButton>
+				<div className="flex flex-col gap-6">
+					<p>Zapoznaj się z naszą galerią, aby zobaczyć jak wygląda nasza sala bankietowa.</p>
+					<LinkButton href="/galeria" className="self-start">
+						Zobacz całą galerię
+					</LinkButton>
 				</div>
 			</article>
+			<div className="lg:basis-1/2">
+				<PhotoGallery photos={GALLERY_IMAGES.slice(0, 6)} />
+			</div>
 		</IntroSectionContainer>
 	);
 };
