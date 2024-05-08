e70d36766e96172545f8bbacefdcd72cb4a0250d
Michal Lata <michallata.dev@gmail.com>
2024-05-08 15:51:33 +0200

    patch: Update LinkButton styles

--- src/components/common/LinkButton.tsx
+++ src/components/common/LinkButton.tsx
@@ -13,7 +13,7 @@ export const LinkButton = ({ href, children, className = "", ...props }: TLinkBu
 		<Link
 			href={href}
 			className={clsx(
-				"rounded-sm border border-brand-primary border-opacity-60 bg-brand-primary px-6 py-3 text-center font-bold text-white shadow-sm transition-colors hover:bg-white hover:text-brand-primary",
+				"rounded-sm border border-brand-primary-light border-opacity-60 bg-brand-primary px-6 py-3 text-center font-bold text-white shadow-sm transition-colors hover:bg-white hover:text-brand-primary-light",
 				className,
 			)}
 			{...props}
