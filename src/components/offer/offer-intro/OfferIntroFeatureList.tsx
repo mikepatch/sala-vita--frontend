3f9541f0edcef96ad58cbe8502d3a1c84a4b50eb
Michal Lata <michallata.dev@gmail.com>
2024-05-08 15:50:22 +0200

    patch: Update OfferIntroFeatureList

--- src/components/offer/offer-intro/OfferIntroFeatureList.tsx
+++ src/components/offer/offer-intro/OfferIntroFeatureList.tsx
@@ -1,13 +1,12 @@
-import { Gem } from "lucide-react";
-
 import { OFFER_ITEMS } from "@/constants";
+import { GemIcon } from "@/components/common/GemIcon";
 
 export const OfferIntroFeatureList = () => {
 	return (
 		<ul className="flex flex-col gap-6">
 			{OFFER_ITEMS.map(({ id, title, shortDescription }) => (
 				<li key={id} className="flex items-center gap-4">
-					<Gem strokeWidth={2} className="" />
+					<GemIcon />
 					<article className="basis-5/6">
 						<header>
 							<h3 className="text-xl font-bold text-brand-primary">{title}</h3>
