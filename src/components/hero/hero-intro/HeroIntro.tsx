f4c9741ed3a39ad4e9a5ad166f3955bd7b834fae
Michal Lata <michallata.dev@gmail.com>
2024-05-10 18:32:58 +0200

    patch: Little refactor of HeroIntro

--- src/components/hero/hero-intro/HeroIntro.tsx
+++ src/components/hero/hero-intro/HeroIntro.tsx
@@ -1,35 +1,20 @@
 import NextImage from "next/image";
-import Link from "next/link";
 
 import logoImg from "../../../../public/images/logo-big.png";
+import heroImg from "../../../../public/images/hero-background.webp";
+import { PageHeroContainer } from "@/components/common/PageHeroContainer";
+import { CtaButton } from "@/components/hero/hero-intro/CtaButton";
 
 export const HeroIntro = () => {
 	return (
-		<section
-			className="z-10 flex h-screen w-full justify-center bg-cover bg-fixed bg-no-repeat text-white"
-			style={{ backgroundImage: "url(/images/hero-background.webp)" }}
-		>
-			<div className="absolute inset-0 bg-gradient-hero" />
-			<article className="flex h-full flex-col items-center justify-center gap-8 py-20 ">
-				<NextImage
-					src={logoImg}
-					alt="Logo"
-					className="w-[250px] animate-fade-up object-cover animation-delay-450 md:w-[400px]"
-					sizes="(max-width: 768px) 250px, 400px"
-				/>
-				<div className="sr-only">
-					<h1>VITA</h1>
-					<p>SALA BANKIETOWA</p>
-				</div>
-				<footer className="flex animate-fade-up flex-col gap-4 animation-delay-850">
-					<Link
-						href="#kontakt"
-						className="rounded-sm bg-white px-6 py-3 text-center text-brand-primary shadow-sm transition-all hover:shadow-md"
-					>
-						Zarezerwuj termin
-					</Link>
-				</footer>
+		<PageHeroContainer image={heroImg} isRoot>
+			<article className="relative z-10 flex h-full flex-col items-center justify-center gap-8 py-20 ">
+				<figure className="relative aspect-[460/470] w-[250px] md:w-[400px]">
+					<NextImage src={logoImg} alt="Logo" fill priority />
+				</figure>
+				<h1 className="sr-only">Sala bankietowa Vita</h1>
+				<CtaButton />
 			</article>
-		</section>
+		</PageHeroContainer>
 	);
 };
