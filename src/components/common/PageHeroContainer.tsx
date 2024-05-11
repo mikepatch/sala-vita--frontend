64603cac17f919000e5d936724a7301294d96432
Michal Lata <michallata.dev@gmail.com>
2024-05-11 13:55:23 +0200

    feat: Add PageHeroContainer

---
+++ src/components/common/PageHeroContainer.tsx
@@ -0,0 +1,38 @@
+import clsx from "clsx";
+import NextImage, { type StaticImageData } from "next/image";
+
+type PageHeroProps = {
+	children: React.ReactNode;
+	image: StaticImageData;
+	isRoot?: boolean;
+};
+
+export const PageHeroContainer = ({ children, image, isRoot = false }: PageHeroProps) => {
+	return (
+		<section
+			className={clsx(
+				"relative z-10 flex h-[100dvh] w-full justify-center overflow-hidden text-brand-primary",
+				{
+					"h-[50dvh] pt-navbar": !isRoot,
+				},
+			)}
+			style={{ clipPath: "inset(0 0 0 0)" }}
+		>
+			<figure className="fixed left-0 top-0 -z-10 h-full w-full">
+				<NextImage
+					src={image}
+					fill
+					placeholder="blur"
+					alt="Hero background image"
+					sizes="100vw"
+					loading="eager"
+					style={{
+						objectFit: "cover",
+					}}
+				/>
+			</figure>
+			<div className="absolute inset-0 -mb-1 bg-gradient-hero" />
+			{children}
+		</section>
+	);
+};
