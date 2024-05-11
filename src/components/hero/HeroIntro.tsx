0c868864afbfdbfe9bcea4b22c9ecc23f1ac68f2
Michal Lata <michallata.dev@gmail.com>
2024-05-11 13:53:46 +0200

    patch: Update HeroIntro

--- src/components/hero/HeroIntro.tsx
+++ src/components/hero/HeroIntro.tsx
@@ -1,16 +1,16 @@
 import NextImage from "next/image";
 
-import logoImg from "../../../../public/images/logo-big.png";
-import heroImg from "../../../../public/images/hero-background.webp";
+import logoImg from "../../../public/images/logo-big.png";
+import heroImg from "../../../public/images/sala-bankietowa-vita-2.jpg";
 import { PageHeroContainer } from "@/components/common/PageHeroContainer";
-import { CtaButton } from "@/components/hero/hero-intro/CtaButton";
+import { CtaButton } from "@/components/hero/CtaButton";
 
 export const HeroIntro = () => {
 	return (
 		<PageHeroContainer image={heroImg} isRoot>
 			<article className="relative z-10 flex h-full flex-col items-center justify-center gap-8 py-20 ">
 				<figure className="relative aspect-[460/470] w-[250px] md:w-[400px]">
-					<NextImage src={logoImg} alt="Logo" fill priority />
+					<NextImage src={logoImg} alt="Logo" fill style={{ objectFit: "contain" }} priority />
 				</figure>
 				<h1 className="sr-only">Sala bankietowa Vita</h1>
 				<CtaButton />
