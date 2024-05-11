0fc76c1246ece0f356bd0b4f2dcfd0554ea8bdac
Michal Lata <michallata.dev@gmail.com>
2024-05-11 13:56:34 +0200

    patch: Update styles and add animations in IntroSectionContainer

--- src/components/common/IntroSectionContainer.tsx
+++ src/components/common/IntroSectionContainer.tsx
@@ -1,25 +1,43 @@
+"use client";
+
 import clsx from "clsx";
+import { motion } from "framer-motion";
 
 type IntroSectionContainerProps = {
 	children: React.ReactNode;
 	id?: string;
 	layout?: "column" | "row" | "row-reverse";
+	className?: string;
 };
 
 export const IntroSectionContainer = ({
 	children,
 	id,
 	layout = "column",
+	className,
 }: IntroSectionContainerProps) => {
 	return (
-		<section
+		<motion.section
+			initial={{
+				opacity: 0,
+				y: 80,
+			}}
+			whileInView={{
+				opacity: 1,
+				y: 0,
+				transition: {
+					duration: 0.8,
+				},
+			}}
+			viewport={{ once: true, amount: 0.01 }}
 			id={id}
-			className={clsx(
-				"container flex flex-col gap-6  xl:gap-12",
-				layout && `xl:flex-${layout} xl:justify-between`,
-			)}
+			className={clsx("container flex flex-col gap-8 lg:gap-12", className, {
+				"justify-between lg:flex-row": layout === "row",
+				"justify-between lg:flex-row-reverse": layout === "row-reverse",
+				"lg:flex-col": layout === "column",
+			})}
 		>
 			{children}
-		</section>
+		</motion.section>
 	);
 };
