caa82482c95e80265a15d34ebf1263ca34214eb0
Michal Lata <michallata.dev@gmail.com>
2024-05-13 15:55:56 +0200

    patch: Update PageHeroContainer with dvh

--- src/components/common/PageHeroContainer.tsx
+++ src/components/common/PageHeroContainer.tsx
@@ -11,28 +11,30 @@ export const PageHeroContainer = ({ children, image, isRoot = false }: PageHeroP
 	return (
 		<section
 			className={clsx(
-				"relative z-10 flex h-[100svh] w-full justify-center overflow-hidden text-brand-primary",
+				"relative z-10 flex h-[100dvh] w-full justify-center overflow-hidden text-brand-primary",
 				{
-					"max-h-[50svh] pt-navbar": !isRoot,
+					"max-h-[50dvh] pt-navbar": !isRoot,
 				},
 			)}
 			style={{ clipPath: "inset(0 0 0 0)" }}
 		>
-			<figure className="fixed left-0 top-0 -z-10 h-full w-full">
-				<NextImage
-					src={image}
-					fill
-					placeholder="blur"
-					alt="Hero background image"
-					sizes="100vw"
-					loading="eager"
-					style={{
-						objectFit: "cover",
-						objectPosition: "center",
-					}}
-				/>
-			</figure>
-			<div className="absolute inset-0 -mb-1 bg-gradient-hero" />
+			<div className="">
+				<figure className="fixed left-0 top-0 -z-10 h-full w-full">
+					<NextImage
+						src={image}
+						fill
+						placeholder="blur"
+						alt="Hero background image"
+						sizes="100vw"
+						loading="eager"
+						style={{
+							objectFit: "cover",
+							objectPosition: "center",
+						}}
+					/>
+				</figure>
+				<div className="absolute inset-0 -mb-1 bg-gradient-hero" />
+			</div>
 			{children}
 		</section>
 	);
