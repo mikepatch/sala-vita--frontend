d58442cc7d99e75c50ab790e251156f658cdb4e3
Michal Lata <michallata.dev@gmail.com>
2024-05-14 17:59:11 +0200

    feat: Implement GoogleMaps in ContactIntro and ContactPageContent

--- src/components/contact/ContactIntro.tsx
+++ src/components/contact/ContactIntro.tsx
@@ -1,11 +1,12 @@
 import { IntroSectionContainer } from "@/components/common/IntroSectionContainer";
 import { IntroSectionHeader } from "@/components/common/IntroSectionHeader";
+import { GoogleMaps } from "@/components/common/GoogleMaps";
 import { ContactItemList } from "@/components/contact/ContactItemList";
 
 export const ContactIntro = () => {
 	return (
 		<IntroSectionContainer layout="row-reverse">
-			<article className="flex w-full flex-col px-8 lg:max-w-lg">
+			<article className="flex w-full flex-col px-8 pb-8 lg:max-w-lg">
 				<IntroSectionHeader title="Kontakt" />
 				<div className="flex flex-col gap-6">
 					<p>
@@ -15,17 +16,9 @@ export const ContactIntro = () => {
 					<ContactItemList direction="column" />
 				</div>
 			</article>
-			<iframe
-				title="Mapa dojazdu do sali bankietowej VITA"
-				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2528.9121098341802!2d19.06817547706106!3d50.665892371827546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4710ccf9a6ccc40f%3A0xc6d5eb7618edb214!2sGminna%204%2C%2042-261%20Starcza!5e0!3m2!1spl!2spl!4v1713545380060!5m2!1spl!2spl"
-				width="800"
-				height="600"
-				style={{ border: 0 }}
-				loading="lazy"
-				allowFullScreen
-				referrerPolicy="no-referrer-when-downgrade"
-				className="max-w-full overflow-hidden lg:basis-1/2 lg:rounded-sm"
-			/>
+			<div className="max-w-full overflow-hidden lg:basis-1/2 lg:rounded-sm">
+				<GoogleMaps />
+			</div>
 		</IntroSectionContainer>
 	);
 };
--- src/components/contact/ContactPageContent.tsx
+++ src/components/contact/ContactPageContent.tsx
@@ -1,6 +1,7 @@
 import { ContactItemList } from "@/components/contact/ContactItemList";
 import { PageSectionContainer } from "@/components/common/PageSectionContainer";
 import { Heading } from "@/components/common/Heading";
+import { GoogleMaps } from "@/components/common/GoogleMaps";
 
 export const ContactPageContent = () => {
 	return (
@@ -15,17 +16,9 @@ export const ContactPageContent = () => {
 				</Heading>
 				<ContactItemList direction="row" />
 			</PageSectionContainer>
-			<iframe
-				title="Mapa dojazdu do sali bankietowej VITA"
-				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2528.9121098341802!2d19.06817547706106!3d50.665892371827546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4710ccf9a6ccc40f%3A0xc6d5eb7618edb214!2sGminna%204%2C%2042-261%20Starcza!5e0!3m2!1spl!2spl!4v1713545380060!5m2!1spl!2spl"
-				width="800"
-				height="600"
-				style={{ border: 0 }}
-				loading="lazy"
-				allowFullScreen
-				referrerPolicy="no-referrer-when-downgrade"
-				className="h-[400px] w-full max-w-full overflow-hidden"
-			/>
+			<div className="h-[400px] w-full max-w-full overflow-hidden">
+				<GoogleMaps />
+			</div>
 		</>
 	);
 };
