683f809916ccd501ed2fec3d85427fd856d95847
Michal Lata <michallata.dev@gmail.com>
2024-05-17 13:04:11 +0200

    feat: Add OGImageLogoMini

---
+++ src/components/common/OGImage/OGImageLogoMini.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
+
+export const OGImageLogoMini = () => {
+	return (
+		<img
+			src={`${process.env.NEXT_PUBLIC_BASE_URL}/images/logo-mini.png`}
+			alt="Sala bankietowa VITA"
+			style={{ objectFit: "cover", height: "200px" }}
+		/>
+	);
+};
