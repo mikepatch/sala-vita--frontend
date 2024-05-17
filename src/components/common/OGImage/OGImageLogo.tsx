fcc4619582fc08eaee779becb201c1436af19bfd
Michal Lata <michallata.dev@gmail.com>
2024-05-17 13:04:03 +0200

    feat: Add OGImageLogo

---
+++ src/components/common/OGImage/OGImageLogo.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
+
+export const OGImageLogo = () => {
+	if (process.env.NEXT_PUBLIC_BASE_URL === undefined) {
+		throw new Error("NEXT_PUBLIC_BASE_URL is not defined");
+	}
+
+	return (
+		<img
+			src={`${process.env.NEXT_PUBLIC_BASE_URL}/images/logo-big.png`}
+			alt="Sala bankietowa VITA"
+			style={{
+				objectFit: "contain",
+				height: "150px",
+				position: "absolute",
+				left: "30px",
+				top: "30px",
+			}}
+		/>
+	);
+};
