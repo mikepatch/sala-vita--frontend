2535198ce2f502a5c6c476a035bee5519bceb4eb
Michal Lata <michallata.dev@gmail.com>
2024-05-17 13:03:51 +0200

    feat: Add OGImageBackground

---
+++ src/components/common/OGImage/OGImageBackground.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @next/next/no-img-element */
+
+export const OGImageBackground = () => {
+	if (process.env.NEXT_PUBLIC_BASE_URL === undefined) {
+		throw new Error("NEXT_PUBLIC_BASE_URL is not defined");
+	}
+
+	return (
+		<figure
+			style={{
+				position: "absolute",
+				top: 0,
+				left: 0,
+				height: "100%",
+				width: "100%",
+			}}
+		>
+			<img
+				alt="Sala bankietowa VITA"
+				src={`${process.env.NEXT_PUBLIC_BASE_URL}/images/sala-bankietowa-vita-2.jpg`}
+				style={{
+					height: "100%",
+					objectFit: "cover",
+					width: "100%",
+				}}
+			/>
+			<div
+				style={{
+					backgroundImage:
+						"linear-gradient(0deg, rgba(251,250,245,1) 0%, rgba(251,250,245,0.6) 30%)",
+					position: "absolute",
+					top: 0,
+					left: 0,
+					width: "100%",
+					height: "100%",
+				}}
+			/>
+		</figure>
+	);
+};
