508404694cd1f16b8b7a224f081ed9762f8f3b32
Michal Lata <michallata.dev@gmail.com>
2024-08-31 17:58:41 +0200

    patch: Change Google Maps implementation

---
+++ src/components/common/LocationMap.tsx
@@ -0,0 +1,12 @@
+export const LocationMap = () => {
+	return (
+		<iframe
+			title="Mapa dojazdu"
+			src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d569.1414774735723!2d19.070409033871286!3d50.66561010293081!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4710cd058daf3e5d%3A0x57deaed6d12a912b!2sSala%20Bankietowa%20Vita!5e0!3m2!1spl!2spl!4v1725117915705!5m2!1spl!2spl"
+			allowFullScreen={true}
+			loading="lazy"
+			referrerPolicy="no-referrer-when-downgrade"
+			className="h-full w-full"
+		></iframe>
+	);
+};
